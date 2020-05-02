daff6b72f82c57689aeeffec9109a61006c3fc23
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-05-01 19:04:46 -0700

    Install and Test SCSS

--- versions/v4/src/sections/AboutSection.vue
+++ versions/v4/src/sections/AboutSection.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <p>About</p>
+  <div class="about">
+    <p class="about__title">About</p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "@/scss/_variables.scss";
+.about {
+  &__title {
+    color: $test-colour;
+  }
+}
+</style>
