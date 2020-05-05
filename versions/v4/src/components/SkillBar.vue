5a0a5718b09e68df3a456b6721b7c1926e7cb5d3
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-05-04 18:40:26 -0700

    Make SkillBar Component

---
+++ versions/v4/src/components/SkillBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ name + ": " + level }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    level: Number,
+    categories: Array
+  },
+  computed: {
+    colored() {
+      return (this.level > 50);
+    }
+  }
+}
+</script>
--- versions/v4/src/sections/SkillsSection.vue
+++ versions/v4/src/sections/SkillsSection.vue
@@ -1,14 +1,18 @@
 <template>
   <Section id="skills" theme="light">
     Skills go Here
+    <SkillBar name="Java" level=75 categories="['programming']" />
+    <SkillBar name="JavaScript" level=80 categories="['programming', 'web']" />
   </Section>
 </template>
 
 <script>
 import Section from "@/components/Section.vue";
+import SkillBar from "@/components/SkillBar.vue";
 export default {
   components: {
-    Section
+    Section,
+    SkillBar
   }
 }
 </script>
