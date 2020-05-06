77027b5f68ef81845134e897187d8d6ac637d61f
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-05-05 23:15:13 -0700

    Make InterestCard Component

---
+++ versions/v4/src/components/InterestCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="interest-card" :class="`interest-card--${this.position}`">
+    {{ name }}
+    <div v-if="this.position === 'mid'" class="interest-card__details">
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    imgsrc: String,
+    description: String,
+    link: Object, // label, url
+    position: String // left, mid, right
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/_variables.scss";
+
+.interest-card {
+  background-color: $color-blue-lightest;
+  border-radius: $border-radius-default;
+  width: 200px;
+  height: 200px;
+
+  &--mid {
+    width: 400px;
+    height: 300px;
+  }
+
+  &--left {
+    // move slightly right
+  }
+
+  &--right {
+    // move slightly left
+  }
+
+  &__details {
+    // only for mid
+  }
+}
+</style>
--- versions/v4/src/sections/InterestsSection.vue
+++ versions/v4/src/sections/InterestsSection.vue
@@ -1,18 +1,67 @@
 <template>
   <Section id="interests" theme="light">
-    My Interests
+    <div class="interests-wrapper">
+      <InterestCard
+        :name="this.interests[indexLeft]['name']"
+        :imgsrc="this.interests[indexLeft]['imgsrc']"
+        position="left"
+      />
+      <InterestCard
+        :name="this.interests[indexMid]['name']"
+        :imgsrc="this.interests[indexMid]['imgsrc']"
+        :description="this.interests[indexMid]['description']"
+        :link="this.interests[indexMid]['link']"
+        position="mid"
+      />
+      <InterestCard
+        :name="this.interests[indexRight]['name']"
+        :imgsrc="this.interests[indexRight]['imgsrc']"
+        position="right"
+      />
+    </div>
   </Section>
 </template>
 
 <script>
 import Section from "@/components/Section.vue";
+import InterestCard from "@/components/InterestCard.vue";
+import interestsData from "@/data/interests.json";
 export default {
   components: {
-    Section
+    Section,
+    InterestCard
+  },
+  data() {
+    return {
+      indexLeft: 0,
+      numInterests: interestsData.interests.length,
+      interests: interestsData.interests
+    }
+  },
+  computed: {
+    indexMid() {
+      return (this.indexLeft + 1) % this.numInterests;
+    },
+    indexRight() {
+      return (this.indexLeft + 2) % this.numInterests;
+    }
+  },
+  methods: {
+    clicked(side) {
+      if (side === "right") {
+        indexLeft = (indexLeft + 1) % this.numInterests;
+      } else if (side === "left") {
+        indexLeft = (indexLeft - 1) % this.numInterests;
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "@/scss/_variables.scss";
+
+.interests-wrapper {
+  display: flex;
+}
 </style>
