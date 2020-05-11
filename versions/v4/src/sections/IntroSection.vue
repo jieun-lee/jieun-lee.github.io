b898853344cb35a7301462a72540b28a201509a7
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-05-10 20:32:12 -0700

    Add Intro Background Image

--- versions/v4/src/sections/IntroSection.vue
+++ versions/v4/src/sections/IntroSection.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="section" id="intro">
-    <p class="section__title">Intro</p>
+  <div class="section intro" id="intro">
+    <div class="intro__wrapper">
+      Intro Section
+    </div>
+    <img class="intro__background" src="@/assets/introbg.jpg" />
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "@/scss/_variables.scss";
-#intro {
-  min-height: 200px;
-  background-color: $color-green-100;
+.intro {
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  background-color: $color-green-300; // when img doesn't load
+  border-bottom: 8px solid $color-green-700;
+
+  &__background {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: left;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  &__wrapper {
+    position: relative;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
