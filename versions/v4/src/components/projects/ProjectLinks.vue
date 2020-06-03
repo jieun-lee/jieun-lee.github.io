02df8b4413af7e691b32c089c277538248ee8a08
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-06-02 18:56:28 -0700

    Change Project Link Icons

--- versions/v4/src/components/projects/ProjectLinks.vue
+++ versions/v4/src/components/projects/ProjectLinks.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="project-links">
-      <a
-        v-if="this.code !== ''"
-        :href="this.code"
-        class="project-links__icon project-links__icon--code"
-        target="_blank"
-      >C</a>
-      <a
-        v-if="this.run !== ''"
-        :href="this.run"
-        class="project-links__icon project-links__icon--run"
-        target="_blank"
-      >R</a>
+      <a v-if="this.code !== ''" :href="this.code" class="project-links__icon" target="_blank">
+        <span class="project-links__icon__text project-links__icon__text--code">&lt;/&gt;</span>
+      </a>
+      <a v-if="this.run !== ''" :href="this.run" class="project-links__icon" target="_blank">
+        <span class="project-links__icon__text project-links__icon__text--run">&#x25B6;</span>
+      </a>
   </div>
 </template>
 
@@ -34,6 +28,8 @@ export default {
 <style lang="scss" scoped>
 @import "@/scss/_variables.scss";
 
+$icon-dimensions: 32px;
+
 .project-links {
   display: flex;
   place-content: flex-end;
@@ -43,12 +39,29 @@ export default {
     // later to be replaced with icons
     background-color: $color-green-500;
     border-radius: $border-radius-extra;
-    padding: $spacing-xsmall $spacing-xsmall*2.5;
+    width: $icon-dimensions;
+    height: $icon-dimensions;
     text-align: center;
     text-decoration: none;
-    color: $color-component-bg;
-    font-weight: 600;
     margin-left: $spacing-xsmall;
+    position: relative;
+
+    &__text {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      color: $color-component-bg;
+      font-size: $font-size-xsmall;
+      font-weight: 800;
+
+      &--code {
+        transform: translate(-50%, -55%);
+      }
+
+      &--run {
+        transform: translate(-35%, -50%);
+      }
+    }
 
     &:hover {
       background-color: $color-green-600;
